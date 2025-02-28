7423b10997d12a63bc3c5485d4d5b47b25c2a453
”Messmer” <2429611912@qq.com>
2025-02-28 14:08:27 +0800

    update

--- src/SliceoHeaven.java
+++ src/SliceoHeaven.java
@@ -1,15 +1,44 @@
 public class SliceoHeaven {
-    public String storeName;
-    public String storeAddress;
-    public String storeEmail;
-    public String storePhone;
-    public String storeMenu;
+    public String storeName, storeAddress, storeEmail, storePhone, storeMenu; 
     public String pizzaIngredients;
     public double pizzaPrice;
-    public String sides;
-    public String drinks;
-    public String orderID;
-    public double orderTotal;
+    public String sides, drinks;
+    private String orderID;
+    private double orderTotal;
+    private static final String DEF_ORDER_ID = "DEF - SOH - 099";
+    private static final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
+    private static final double DEF_ORDER_TOTAL = 15.00;
+
+    public SliceoHeaven() {
+        this.orderID = DEF_ORDER_ID;
+        this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
+        this.orderTotal = DEF_ORDER_TOTAL;
+        this.sides = "";
+        this.drinks = "";
+    }
+
+    public SliceoHeaven(String orderID, String pizzaIngredients, double orderTotal) {
+        this.orderID = orderID;
+        this.pizzaIngredients = pizzaIngredients;
+        this.orderTotal = orderTotal;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public double getOrderTotal() {
+        return orderTotal;
+    }
+
+    public void setOrderTotal(double orderTotal) {
+        this.orderTotal = orderTotal;
+    }
+
     public void takeOrder(String id, String ingredients, double total) {
         orderID = id;
         pizzaIngredients = ingredients;
@@ -25,16 +54,15 @@ public class SliceoHeaven {
         printReceipt();
     }
 
-    
     public void makePizza() {
         System.out.println("Making pizza with ingredients: " + pizzaIngredients);
     }
 
-    
-    public void printReceipt() {
+    private void printReceipt() {
         System.out.println("********RECEIPT********");
         System.out.println("Order ID: " + orderID);
         System.out.println("Pizza Ingredients: " + pizzaIngredients);
         System.out.println("Order Total: " + orderTotal);
     }
 }
+  
